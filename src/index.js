1e62ada88dc8856515d538720ceea07de7b4cc36
Raja0514 <rajamr1987@gmail.com>
2022-02-23 04:10:53 +0400

    titlle

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import "./Index.css";
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Function with Props</h1>
+    <h1>Function Component with Props</h1>
     <App name="Oman" location="Qurum" city="ruwi" mallname="carefour"></App>
   </React.StrictMode>,
   document.getElementById("root")
